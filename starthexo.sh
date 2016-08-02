4a75581787a5f15dfdf91790024968ec2cb821c0
zaozaool.j <mr_jooe@126.com>
2016-08-02 19:29:31 +0800

    Update starthexo.sh

--- starthexo.sh
+++ starthexo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cd /home/ubuntu/repositories/zaozaool.github.io.hexo
+cd ~/repositories/zaozaool.github.io.hexo
 
 hexo clean
 
-nohup node /home/ubuntu/cloud9/c9sdk/server.js -p 8181 -l 0.0.0.0 -a : -w /home/ubuntu/repositories/zaozaool.github.io.hexo > hexo.out 2>&1 &
+nohup node ~/cloud9/c9sdk/server.js -p 8181 -l 0.0.0.0 -a : -w ~/repositories/zaozaool.github.io.hexo > hexo.out 2>&1 &
